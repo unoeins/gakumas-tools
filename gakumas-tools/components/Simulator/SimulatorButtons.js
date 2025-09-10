7e2f0040b4d7a71f4eabf2777a169be73b8dcf46
unoeins <twintailsister@gmail.com>
2025-09-11 05:12:34 +0900

    Add Read URL button and remove manage loadouts button

--- gakumas-tools/components/Simulator/SimulatorButtons.js
+++ gakumas-tools/components/Simulator/SimulatorButtons.js
@@ -1,20 +1,41 @@
 "use client";
 import { memo, useContext, useState } from "react";
 import { useTranslations } from "next-intl";
-import { FaCheck, FaRegCopy } from "react-icons/fa6";
+import { FaCheck, FaRegCopy, FaRegPaste } from "react-icons/fa6";
 import Button from "@/components/Button";
 import ConfirmModal from "@/components/ConfirmModal";
 import LoadoutManagerModal from "@/components/LoadoutManagerModal";
 import LoadoutContext from "@/contexts/LoadoutContext";
 import ModalContext from "@/contexts/ModalContext";
+import { loadoutFromSearchParams } from "@/utils/simulator";
 import styles from "./Simulator.module.scss";
 
 function SimulatorButtons() {
   const t = useTranslations("SimulatorButtons");
 
-  const { clear, clearOrders, simulatorUrl } = useContext(LoadoutContext);
+  const { clear, clearOrders, simulatorUrl, setLoadout } = useContext(LoadoutContext);
   const { setModal } = useContext(ModalContext);
   const [linkCopied, setLinkCopied] = useState(false);
+
+  async function readLoadoutFromUrl() {
+    try {
+      const text = await navigator.clipboard.readText();
+      if (!/^https?:\/\/.+/i.test(text)) {
+        setModal(<ConfirmModal message={t("invalidUrl")} showCancel={false} />);
+        return;
+      }
+      const url = new URL(text);
+      const loadout = loadoutFromSearchParams(url.searchParams);
+      if (loadout.hasDataFromParams) {
+        setModal(<ConfirmModal message={t("confirmSetLoadout")} onConfirm={() => setLoadout(loadout)} />);
+      } else {
+        setModal(<ConfirmModal message={t("invalidLoadout")} showCancel={false} />);
+      }
+    } catch (err) {
+      console.error("Clipboard error:", err);
+      setModal(<ConfirmModal message={t("clipboardError")} showCancel={false} />);
+    }
+  }
 
   return (
     <div className={styles.buttons}>
@@ -38,13 +59,6 @@ function SimulatorButtons() {
 
       <Button
         style="blue-secondary"
-        onClick={() => setModal(<LoadoutManagerModal />)}
-      >
-        {t("manageLoadouts")}
-      </Button>
-
-      <Button
-        style="blue-secondary"
         onClick={() => {
           navigator.clipboard.writeText(simulatorUrl);
           setLinkCopied(true);
@@ -59,6 +73,14 @@ function SimulatorButtons() {
             URL
           </>
         )}
+      </Button>
+
+      <Button
+        style="blue-secondary"
+        onClick={() => readLoadoutFromUrl()}
+      >
+        <FaRegPaste />
+        {t("readLoadoutFromUrl")}
       </Button>
     </div>
   );
